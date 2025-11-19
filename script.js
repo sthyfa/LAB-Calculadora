ad5e799ab6cbc64ec4ef0daac828fcdac24b5a9e
Fancy Encinas <77sthy.fa@gmail.com>
2025-11-19 13:28:51 -0600

    feat: Archivo HTML y vincular con .js

---
+++ script.js
@@ -0,0 +1,29 @@
+//INPUT
+
+//Proceso
+function sumar(a, b){
+    let suma = a + b;
+    return suma;
+}
+
+function restar(a, b){
+    let resta = a - b;
+    return resta;
+}
+
+function multiplicar(a, b){
+    let multiplicacion = a * b;
+    return multiplicacion;
+}
+
+function dividir(a, b){
+    let division = a / b;
+    return division;
+}
+
+function calculadora(a, b, operacion){
+    
+}
+
+
+//OUTPUT
